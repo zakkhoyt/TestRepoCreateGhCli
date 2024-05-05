56272f9e6b333b4b40279e08bf97ed81cfa751d8
zakkhoyt <zakkhoyt@hatchbaby.com>
2024-05-04 20:30:22 -0600

    init commit

---
+++ Tests/gh_repo_createTests/gh_repo_createTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import gh_repo_create
+
+final class gh_repo_createTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
